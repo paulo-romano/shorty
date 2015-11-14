456bb7d6a9ea6183f38b5dcc26a0da16bb59d182
Paulo Romano <pauloromanocarvalho@gmail.com>
2015-11-13 22:40:26 -0200

    Base layout and bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, request, redirect
+from flask_bootstrap import Bootstrap
 
-app = Flask('shorty')
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+    app.config.from_pyfile('config.py')
 
+    return app
+
+app = create_app()
 
 @app.route('/')
 def index():
-    return 'shorty'
+    return render_template('index.html')
+
+@app.route('/shortit', methods=['POST'])
+def shortit():
+    flash(request.form['url'] + ' foi encurtada!', 'success')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
